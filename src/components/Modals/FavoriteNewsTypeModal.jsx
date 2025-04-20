11b6dcd0f66359a93adf10f91af23cc5c22784a7
Mubashir Liaqat <161022598+Mubashir1292@users.noreply.github.com>
2025-04-20 14:48:21 -0700

    implemented till the ai types whereae selections of types and filteringis remaining.

--- src/components/Modals/FavoriteNewsTypeModal.jsx
+++ src/components/Modals/FavoriteNewsTypeModal.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { FiX } from 'react-icons/fi';
+import { useSelector } from 'react-redux';
 const FavoriteNewsTypeModal=({isOpen,Close})=>{
     const[loading,setLoading]=React.useState(false);
     const[currentText,setCurrentText]=React.useState("");
     // Initial texts
-    const initialText = 'AI is generating personalized news recommendations for you...';
-    const finalText = 'AI has generated your personalized news feed!';
-    const[randomNewsType,setRandomNewsType]=React.useState([
-        
-    ]);
+    const initialText = 'AI is generating personalized news recommendations Types for you...';
+    const finalText = 'Please Choose your favorite news type from the list below:';
+    const newsFilters=useSelector(state=>state.newsFilter.newsFilters);
+    const [selectedCategories,setSelectedCategories]=React.useState([]);
+    const handleCategoryClick=(category)=>{
+        console.log(category);
+    }
     React.useEffect(()=>{
         if(!isOpen) return;
         // setting current text to currentText state
@@ -19,20 +22,23 @@ const FavoriteNewsTypeModal=({isOpen,Close})=>{
             setLoading(false);
         },4000);
         return()=>{
-            clearTimeout(secondTextTimeout);
             clearTimeout(finalTextTimeout);
         }
     },[isOpen]);
+    const handleClose=()=>{
+        Close();
+    }
     return (
         <React.Fragment>
             <div className={`fixed inset-0 bg-black transition-opacity z-10 duration-500 ${isOpen ? "bg-opacity-90 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
-            onClick={Close}>
-                <div className={`fixed left-1/2 top-1/4 -translate-x-1/2 -translate-y-1/2 w-full max-w-screen-lg h-[40vh] bg-white shadow-xl overflow-y-auto z-100 rounded-lg transition-full duration-500 ease-in-out ${isOpen ? "opacity-1000 scale-100 pointer-events-auto":"opacity-0 scale-95 pointer-events-none"}`}>
+                   onClick={handleClose}
+                >
+                <div className={`fixed left-1/2 top-1/4 -translate-x-2/4 -translate-y-1/4 w-full max-w-screen-lg h-[92vh] bg-white shadow-xl overflow-y-auto z-100 rounded-lg transition-full duration-500 ease-in-out ${isOpen ? "opacity-1000 scale-100 pointer-events-auto":"opacity-0 scale-95 pointer-events-none"}`} onClick={(e)=>e.stopPropagation()}>
                     {/* button to close the modal */}
-                <div className="flex justify-end items-center p-2">
+                <div className="flex justify-end items-center  p-2">
                     <button 
-                        onClick={Close} 
-                        className="p-1 rounded-full hover:bg-gray-100 focus:outline-none"
+                        onClick={handleClose} 
+                        className="p-1 rounded-full  hover:bg-gray-100 focus:outline-none"
                         aria-label="Close modal"
                     >
                         <FiX className="text-2xl" />
@@ -45,8 +51,13 @@ const FavoriteNewsTypeModal=({isOpen,Close})=>{
                                     {currentText}
                                 </h1>
                             </div>
-                        {loading && (<div className={`w-8 h-8 rounded-full border-black border-4 border-t-transparent animate-spin`}></div>)}
-                        
+                        {loading ? (<div className={`w-8 h-8 rounded-full border-black border-4 border-t-transparent animate-spin`}></div>):
+                            <div className="grid grid-cols-3 gap-4 p-2">
+                                {newsFilters && newsFilters.map((type,index)=>(
+                                    <span key={index} className="p-4 cursor-pointer text-center rounded bg-cyan-500 hover:bg-cyan-400 text-white" onClick={()=>handleCategoryClick(type)}>{type.value}</span>
+                                ))}
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
